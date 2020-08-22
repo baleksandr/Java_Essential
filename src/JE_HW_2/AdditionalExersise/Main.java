b8db06b1abc25571eb7579ec59dde992e6e757b2
Aleksandr Borysov <aleksandr.borysov@playtika.com>
2020-08-22 11:59:22 +0300

    Add steps

---
+++ src/JE_HW_2/AdditionalExersise/Main.java
@@ -0,0 +1,8 @@
+package JE_HW_2.AdditionalExersise;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Area: " + MyArea.areaOfCircle(45));
+    }
+}
---
+++ src/JE_HW_2/AdditionalExersise/MyArea.java
@@ -0,0 +1,9 @@
+package JE_HW_2.AdditionalExersise;
+
+public class MyArea {
+   private static final double PI = 3.14;
+
+   public static double areaOfCircle(double radius) {
+       return PI * Math.pow(radius, 2);
+   }
+}
